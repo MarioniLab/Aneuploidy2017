7cd62c4e925face6935987383c64cfd88ef95e35
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-06-25 15:36:56 +0100

    Updated to gitlab file storage

--- get_data.sh
+++ get_data.sh
@@ -4,20 +4,16 @@ if [ ! -e "PaperSupplementary.html" ]
 		echo "There is no PaperSupplementary.html file here - are you running the script from the right place?"
 		echo "If you still want to download here, run 'touch aneu_report.html' and then this script again."
 	else
-		if [ ! -d "raw_data" ]
-			then 
-			mkdir raw_data
-		fi
 
-		if [ ! -d "proc_data" ]
-			then 
-			mkdir proc_data
-		fi
 
-		cd raw_data
-		echo "Beginning ftp"
-		wget --user=jmlabftp --password='HOBICAmeer6' ftp://ftp2.cruk.cam.ac.uk/aneuploidy/aneu_raw.tar.gz
-		echo "Unzipping files"
+		echo "Getting processed data"
+		curl -k https://jmlab-gitlab.cri.camres.org/Jonny/Aneuploidy2017Data/raw/master/aneu_proc.tar.gz > aneu_proc.tar.gz
+		echo "Unzipping processed data"
+		tar -xzf aneu_proc.tar.gz
+
+		echo "Getting raw data"
+		curl -k https://jmlab-gitlab.cri.camres.org/Jonny/Aneuploidy2017Data/raw/master/aneu_raw.tar.gz > aneu_raw.tar.gz
+		echo "Unzipping raw data"
 		tar -xzf aneu_raw.tar.gz
 
 fi
