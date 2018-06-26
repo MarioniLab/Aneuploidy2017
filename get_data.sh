aa20a38884d17c413798d2f2fba6765e89dd6ce7
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-06-26 09:29:09 +0100

    Added data download error note

--- get_data.sh
+++ get_data.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/bash
-if [ ! -e "PaperSupplementary.html" ]
-	then
-		echo "There is no PaperSupplementary.html file here - are you running the script from the right place?"
-		echo "If you still want to download here, run 'touch aneu_report.html' and then this script again."
-	else
+print "Sorry, the server is currently broken, I am working to get the download working again!"
+# if [ ! -e "PaperSupplementary.html" ]
+# 	then
+# 		echo "There is no PaperSupplementary.html file here - are you running the script from the right place?"
+# 		echo "If you still want to download here, run 'touch aneu_report.html' and then this script again."
+# 	else
 
 
-		echo "Getting processed data"
-		curl -k https://jmlab-gitlab.cri.camres.org/Jonny/Aneuploidy2017Data/raw/master/aneu_proc.tar.gz > aneu_proc.tar.gz
-		echo "Unzipping processed data"
-		tar -xzf aneu_proc.tar.gz
+# 		echo "Getting processed data"
+# 		curl -k https://jmlab-gitlab.cri.camres.org/Jonny/Aneuploidy2017Data/raw/master/aneu_proc.tar.gz > aneu_proc.tar.gz
+# 		echo "Unzipping processed data"
+# 		tar -xzf aneu_proc.tar.gz
 
-		echo "Getting raw data"
-		curl -k https://jmlab-gitlab.cri.camres.org/Jonny/Aneuploidy2017Data/raw/master/aneu_raw.tar.gz > aneu_raw.tar.gz
-		echo "Unzipping raw data"
-		tar -xzf aneu_raw.tar.gz
+# 		echo "Getting raw data"
+# 		curl -k https://jmlab-gitlab.cri.camres.org/Jonny/Aneuploidy2017Data/raw/master/aneu_raw.tar.gz > aneu_raw.tar.gz
+# 		echo "Unzipping raw data"
+# 		tar -xzf aneu_raw.tar.gz
 
-fi
+# fi
