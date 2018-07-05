b51710d8da52caf536b2d16afa974fb1221d8a2d
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-07-05 11:19:27 +0100

    Fixed data download

--- get_data.sh
+++ get_data.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/bash
 
-echo "Programmatic download currently has a problem. Please navigate to \"https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/Aneuploidy2017Data\" to download the data."
-
-# if [ ! -e "PaperSupplementary.html" ]
-# 	then
-# 		echo "There is no PaperSupplementary.html file here - are you running the script from the right place?"
-# 		echo "If you still want to download here, run 'touch PaperSupplementary.html' and then this script again."
-# 	else
+if [ ! -e "PaperSupplementary.html" ]
+	then
+		echo "There is no PaperSupplementary.html file here - are you running the script from the right place?"
+		echo "If you still want to download here, run 'touch PaperSupplementary.html' and then this script again."
+	else
 
 
-# 		echo "Getting processed data"
-# 		curl -k https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/Aneuploidy2017Data/raw/master/aneu_proc.tar.gz > aneu_proc.tar.gz
-# 		echo "Unzipping processed data"
-# 		tar -xzf aneu_proc.tar.gz
+		echo "Getting processed data"
+		curl -k https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/Aneuploidy2017Data/raw/master/aneu_proc.tar.gz > aneu_proc.tar.gz
+		echo "Unzipping processed data"
+		tar -xzf aneu_proc.tar.gz
 
-# 		echo "Getting raw data"
-# 		curl -k https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/Aneuploidy2017Data/raw/master/aneu_raw.tar.gz > aneu_raw.tar.gz
-# 		echo "Unzipping raw data"
-# 		tar -xzf aneu_raw.tar.gz
+		echo "Getting raw data"
+		curl -k https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/Aneuploidy2017Data/raw/master/aneu_raw.tar.gz > aneu_raw.tar.gz
+		echo "Unzipping raw data"
+		tar -xzf aneu_raw.tar.gz
 
-# fi
+fi
